709c1db7a6dc6e2ea545988e50d35da40d48f83e
Devi Veytia <deviveytia@hotmail.com>
2024-06-19 16:40:51 +0200

    add plots on mCDR deployments for in person meeting

--- R/my_sec_axis_lineplot.R
+++ R/my_sec_axis_lineplot.R
@@ -1,24 +1,27 @@
-## Plot on second axis by automatically scaling
+#' Plot on second axis by automatically scaling
+#'
+#' @param dat.prim a dataframe with values to be plotted by the first axis, 
+#' where y axis is named value, x axis is named x_axis, colour variable is named col_var
+#' @param dat.sec a dataframe with values to be plotted by the second axis
+#' @param name.prim The name of the first y axis
+#' @param name.sec The name of the second y axis
+#' @param lwd
+#' 
+my_sec_axis_lineplot <- function(dat.prim, dat.sec, name.prim, name.sec, lwd = 1){
+  require(ggplot2)
+  
+  ylim.prim <- range(dat.prim$value, na.rm = T)   
+  ylim.sec <- range(dat.sec$value, na.rm = T)
+  b <- diff(ylim.prim)/diff(ylim.sec)
+  a <- ylim.prim[1] - b*ylim.sec[1]
+  
+  ggp <- ggplot(dat.prim, aes(x = x_axis, y = value, color = col_var))+
+    geom_line(linewidth = lwd)+
+    geom_line(data = dat.sec, aes(x = x_axis, y = a + value*b, color = col_var), inherit.aes = FALSE, linewidth = lwd)+
+    scale_y_continuous(name.prim,sec.axis = sec_axis(~ (. - a)/b, name = name.sec)) +
+    theme_bw()
+  
+  return(ggp)
+  
+}
 
-ylim.prim <- range(oecd_transport_trends_global$value, na.rm = T)   
-ylim.sec <- range(wb_capture_fisheries_global$value, na.rm = T)
-b <- diff(ylim.prim)/diff(ylim.sec)
-a <- ylim.prim[1] - b*ylim.sec[1]
-
-all_volume_global_ggp <- ggplot(oecd_transport_trends_global, 
-                                aes(year, value, color = vehicle_type)) +
-  geom_line() +
-  geom_line(data = wb_capture_fisheries_global,
-            aes(x=year, y = a + value*b, color = vehicle_type), 
-            inherit.aes = FALSE) +
-  scale_y_continuous("Fisheries production\n(metric tons)", 
-                     sec.axis = sec_axis(~ (. - a)/b, name = "Maritime transport\n(Tonnes)")) +
-  scale_x_date("Year") +
-  scale_color_discrete(name="Sector")+
-  labs(caption = "Data sources:\nWorld bank capture fisheries production (tonnes)\nOECD Annual transport trends")+
-  theme_bw()+
-  theme(
-    legend.position = "bottom"
-  )
-
-all_volume_global_ggp
